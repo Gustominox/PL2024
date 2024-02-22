ed639ae3998c42454dd436725bd9c4a4b8447496
Gustominox <a93320@alunos.uminho.pt>
2024-02-22 19:15:19 +0000

    img sub, list sub e link sub (list usa regex dentro da func regex)

--- TPC2/solution.py
+++ TPC2/solution.py
@@ -2,20 +2,20 @@ import sys
 import re
 
 def header_sub(match):
-    print(match)
+    #print(match)
     level = len(match.group(1))  
     conteudo = match.group(2)        
     html = f'<h{level}>{conteudo.strip()}</h{level}>' 
     return html
 
 def bold_sub(match):
-    print(match)
+    #print(match)
     conteudo = match.group(1)        
     html = f'<b>{conteudo.strip()}</b>' 
     return html
 
 def italic_sub(match):
-    print(match)
+    #print(match)
     conteudo = match.group(1)        
     html = f'<i>{conteudo.strip()}</i>' 
     return html
