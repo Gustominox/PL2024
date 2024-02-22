85a2d5b68b59e4bae7e065e9933aa57246d58b62
Gustominox <a93320@alunos.uminho.pt>
2024-02-22 19:22:38 +0000

    solution file unThanos'd

--- TPC2/solution.py
+++ TPC2/solution.py
@@ -20,9 +20,39 @@ def italic_sub(match):
     html = f'<i>{conteudo.strip()}</i>' 
     return html
 
+def ordered_list_sub(match):
+    itens = match.group(0).split('\n')
+    print (match.groups())
+    html = f'<ol>\n'
+    for item in itens:
+        if item != "":
+            m = re.match(r'\d\. (.*)',item)
+            conteudo = m.group(1)
+            html+=f'<li>{conteudo}</li>\n'
+    html+='</ol>\n'
+
+    return html
+
+def link_sub(match):
+    texto = match.group(1)        
+    url = match.group(2)        
+    html = f'<a href="{url}">{texto}</a>' 
+    return html
+
+def img_sub(match):
+    print(match.groups())
+    texto = match.group(1)        
+    path = match.group(2)        
+    html = f'<img src="{path}" alt="{texto}"/>' 
+    return html
+
 file = sys.stdin.read()
 
 file = re.sub(r'^(#+)(.*)', header_sub, file, flags=re.MULTILINE)
 file = re.sub(r'\*\*(.*)\*\*', bold_sub, file, flags=re.MULTILINE)
 file = re.sub(r'\*(.*)\*', italic_sub, file, flags=re.MULTILINE)
+file = re.sub(r'(?:\d\. (.*)\n)+', ordered_list_sub, file, flags=re.MULTILINE)
+file = re.sub(r'!\[(.*)\]\((.*)\)', img_sub, file, flags=re.MULTILINE)
+file = re.sub(r'\[(.*)\]\((.*)\)', link_sub, file, flags=re.MULTILINE)
+
 print(file)
